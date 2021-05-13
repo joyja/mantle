824eabd23fd0f395858612535dfd8fbd96dd1346
James A. Joy <joyja@jarautomation.io>
2021-05-13 20:00:26 +0000

    added MANTLE_HOST and MANTLE_PORT environment variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
         MANTLE_HOST: '0.0.0.0',
-        MANTLE_PORT: 4000
+        MANTLE_PORT: 4000,
       },
     },
   ],
@@ -20,7 +20,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://gitlab.com/joyja/mantle.git',
       path: '/home/ubuntu/mantle',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
-    }
-  }
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+    },
+  },
 }
