adff6c8b7a48af381cafbedbccf4cda7f9911e3d
James A. Joy <joyja@jarautomation.io>
2021-05-13 20:05:00 +0000

    fixed production deploy to use production env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,8 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://gitlab.com/joyja/mantle.git',
       path: '/home/ubuntu/mantle',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+      'post-deploy':
+        'npm install && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 }
