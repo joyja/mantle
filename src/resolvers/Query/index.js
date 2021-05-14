04422df6f4bea215e7d3c9724e64936d915a4326
James A. Joy <joyja@jarautomation.io>
2021-05-14 00:06:37 +0000

    changed name of query function edgeNodes (from EdgeNodes)

--- src/resolvers/Query/index.js
+++ src/resolvers/Query/index.js
@@ -1,8 +1,8 @@
-const EdgeNodes = async function (root, args, context, info) {
+const edgeNodes = async function (root, args, context, info) {
   return context.EdgeNodes
 }
 
 module.exports = {
   info: () => 'Data Acquisition and visualation for tentacle-edge devices.',
-  EdgeNodes,
+  edgeNodes,
 }
